d1825422042fbf8c23429db332c2b5592dd9e182
andresdeinnocentiis <andresdeinnocentiis@gmail.com>
2022-04-14 16:52:42 -0300

    Arreglos 100%

--- js/data/medidasList.js
+++ js/data/medidasList.js
@@ -4,7 +4,7 @@ const liItems = (product) => {
     let medidas = ``
     if(product.medidas.length > 0) {
         for(let medida of product.medidas) {
-            medidas += `<li class='medidas-item-list'>${medida}</li>`
+            medidas += `<li class='medidas-item-list'><span class="medida-text">${medida}<span></li>`
         }
     } 
     return medidas
@@ -12,7 +12,7 @@ const liItems = (product) => {
 
 const hasMedidas = (product) => {
     if(product.medidas.length > 0) {
-        return `<h6 class="detailView-text__medidas">MEDIDAS</h6>` 
+        return `<h6 class="detailView-text__medidas">MEDIDAS:</h6>` 
     } else {
         return ""
     }
